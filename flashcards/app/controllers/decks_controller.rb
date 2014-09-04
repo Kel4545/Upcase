32177c74e0e56f915d2311031d42ccdf0d1576f6
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-09-03 18:36:02 -0600

    Finished video

--- flashcards/app/controllers/decks_controller.rb
+++ flashcards/app/controllers/decks_controller.rb
@@ -4,10 +4,35 @@ class DecksController < ApplicationController
   end
 
   def show
-    @deck = Deck.find(params["id"])
+    @deck = Deck.find(params[:id])
   end
 
   def new
     @deck = Deck.new
+  end
+
+  def create
+    @deck = Deck.new(
+      #Got error trying to do it the way they did in the video
+    name: params[:deck][:name])
+    @deck.save
+    redirect_to "/decks"
+  end
+
+  def edit
+    @deck = Deck.find(params[:id])
+  end
+
+  def update
+    @deck = Deck.find(params[:id])
+    @deck.update_attributes(
+      name: params[:deck][:name])
+    redirect_to "/decks/#{@deck.id}"
+  end
+
+  def destroy
+    @deck = Deck.find(params[:id])
+    @deck.destroy
+    redirect_to "/decks"
   end
 end
